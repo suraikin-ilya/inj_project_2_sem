a11189871ede1963a24bb6e853c16c2638ec1cbf
suraikin-ilya <ilya.suraikin@mail.ru>
2021-03-12 20:39:03 +0300

    Вывод контента

--- article.php
+++ article.php
@@ -1,7 +1,12 @@
+<?php
+$news_id = $_GET['news_id'];
+$mysql = new mysqli('127.0.0.1','root', '','inj_project');
+$news = $mysql->query("SELECT * FROM `news` WHERE `news_id`= '$news_id'")->fetch_array();;
+?>
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
-		<title>$article</title>
+		<title><?php echo $news['heading'];?></title>
 		<link rel="stylesheet" href="css/style.css" />
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway" />
 		<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
--- main_category.php
+++ main_category.php
@@ -11,21 +11,29 @@
             </nav>
         </div>
         <div class="content">
-            <a href="">
-                <div class="category_article">
-                    <h1>Lorem,   elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim</h1>
-                    <img src="./img/img1.png" alt="Не удалось найти изображение">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis. Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis.Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis.</p>
-                    <div class="stats">
-                        <span>12300</span>
-                        <span>900</span>
-                        <span>01.03.2021</span>
-                    </div>
-                </div>
-            </a>
+            <?php
+            $mysql = new mysqli('127.0.0.1','root', '','inj_project');
+            $category_id = $_GET['category'];
+            $news = $mysql->query("SELECT * FROM `news` WHERE `category`= '$category_id'");
+            for($i = 1;$row=$news->fetch_array();$i++)
+            {
+                echo '<a href="article.php?news_id='.$row['news_id'].'"</a>';
+                echo '<div class="category_article">';
+                echo '<h1>'.$row['heading'] .'</h1>';
+                echo '<img src="'.$row['img'].'"> ';
+                echo '<p>'.$row['text'].'</p>';
+                echo '<div class="stats">';
+                echo '<span>12300</span>';
+                echo '<span>900</span>';
+                echo '<span>'.$row['date'].'</span>';
+                echo '</div>';
+                echo '</div>';
+            }
+            ?>
         </div>
         <div class="sidebar">
-
         </div>
     </div>
 </main>
+
+
